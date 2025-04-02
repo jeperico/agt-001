abe0a5af369db5e32f639406fb40731ac20a85e8
jeperico <joaoeperico@gmail.com>
2025-04-02 19:33:53 -0300

    hello, c world

---
+++ c/hello-world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int x;
+    scanf("%i", &x);
+
+    float n1;
+    char k;
+    scanf("&f%c", &n1, &k);
+
+    int name;
+    scanf("%s", &name);
+
+    printf("Name: %s", name)
+}
