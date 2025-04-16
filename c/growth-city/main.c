faecf4cafcde3f55c594bfb90871f5e6b5608902
jeperico <joaoeperico@gmail.com>
2025-04-16 20:36:19 -0300

    growth city

---
+++ c/growth-city/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+  // [CITY A] <Argel> (Argélia)
+  // [CITY B] <Beirute> (Líbano)
+  printf(" +-----------------------------+ \n");
+  printf("<| &-28s |>\n", "QUANTIDADE DE PESSOAS");
+  printf(" +-----------------------------+ \n\n");
+  printf("<| &-28s |>\n", "CITY <Argel>");
+  printf(" +-----------------------------+ \n\n");
+
+  int argelPopulation = 90000;
+  float argelGrowthRate = 0.03;
+  int beirutPopulation = 200000;
+  float beirutGrowthRate = 0.015;
+}
