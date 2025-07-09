9c1cf69e3dcc51bcb282fb5df30446a4c94bc17f
jeperico <joaoeperico@gmail.com>
2025-07-08 21:23:12 -0300

    [feat] fibonacci

---
+++ work-list/fibonacci/fibo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int fibo(int n) {
+  if(n <= 1) return n;
+
+  return fibo(n - 1) + fibo(n - 2);
+}
--- work-list/fibonacci/main.c
+++ work-list/fibonacci/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include "fibo.h"
 
-void main() {
-  
+void main(){
+  int n = 0;
+
+  printf("[ENTER THE NUMBER OF ITERATIONS]: ");
+  scanf("%d", &n);
+
+  for(int i = 0; i < n; i++) {
+    printf("%d ", fibo(i));
+  }
+  printf("\n");
 }
